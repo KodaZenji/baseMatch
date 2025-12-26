2583a1de5490617920c94ca840f1c8b6abae8f49
KodaZenji <2400072.benjamin@nict.edu.ng>
2025-12-26 03:16:28 +0100

    Update wagmi.ts

--- frontend/lib/wagmi.ts
+++ frontend/lib/wagmi.ts
@@ -7,7 +7,7 @@ export const config = createConfig({
   connectors: [
     coinbaseWallet({
       appName: "BaseMatch",
-      preference: "smartWalletOnly",
+      preference: "all",
     }),
   ],
   transports: {
