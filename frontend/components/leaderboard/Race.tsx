41b61a63afa4b02a732f1325b64c5dc545da91f8
KodaZenji <2400072.benjamin@nict.edu.ng>
2026-02-11 12:31:35 +0100

    Refactor Race component layout for RankingsTable

--- frontend/components/leaderboard/Race.tsx
+++ frontend/components/leaderboard/Race.tsx
@@ -258,6 +258,13 @@ export default function Race() {
       
       {/* Main Grid */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-8">
+        <div className="lg:col-span-2">
+          <RankingsTable 
+            gender={gender}
+            setGender={setGender}
+            myWallet={address}
+          />
+        </div>
         
         <div className="lg:col-span-1 space-y-6">
           <CheckInButton walletAddress={address} />
@@ -268,13 +275,6 @@ export default function Race() {
           />
         </div>
         
-        <div className="lg:col-span-2">
-          <RankingsTable 
-            gender={gender}
-            setGender={setGender}
-            myWallet={address}
-          />
-        </div>
         
       </div>
       
